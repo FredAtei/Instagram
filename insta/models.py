f25214e7c2783e999f10fa0aaa94e38cd45b6898
Fred <akamaatei@gmail.com>
2020-11-22 22:26:22 +0300

    add tests

--- insta/models.py
+++ insta/models.py
@@ -9,6 +9,24 @@ class Profile(models.Model):
     profile_picture = CloudinaryField('image')
     bio = HTMLField(blank=True,default='Hey There I am using Instagram')
 
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save() 
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self,new_bio):
+        self.bio = new_bio
+        self.save()
+
+    def update_image(self, user_id, new_image):
+        user = User.objects.get(id = user_id)
+        self.photo = new_image 
+        self.save()             
+
 class Image(models.Model):
     image = CloudinaryField('image')
     name = models.CharField(max_length=60)
@@ -17,10 +35,39 @@ class Image(models.Model):
     likes = models.PositiveIntegerField(default=0,blank=True)
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()    
+
+    def delete_image(self):
+        self.delete()    
+
+    @classmethod
+    def get_images(cls):
+        all_images = cls.objects.all()
+        return all_images 
+    
+    @classmethod
+    def search_by_caption(cls,search_term):
+        post = cls.objects.filter(caption__icontains=search_term)
+        return post
+
+    @classmethod
+    def filter_images_by_user(cls,id):
+        images_by_user = cls.objects.filter(profile = id).all() 
+        return images_by_user    
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ForeignKey('Image', on_delete=models.CASCADE)
     content = HTMLField()
     created = models.DateTimeField(auto_now_add=True)    
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = cls.objects.filter(image_id=id)
+        return comments
     
   
