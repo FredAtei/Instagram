74591e896be791cfcc8ae34e25bdca3f78b176af
Fred <akamaatei@gmail.com>
2020-11-22 20:07:27 +0300

    add profile model and change views

--- insta/admin.py
+++ insta/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Picture
+from .models import Profile
 
 # Register your models here.
 
-admin.site.register(Picture)
+admin.site.register(Profile)
--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
-class Picture(models.Model):
-    image = CloudinaryField('image')
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_picture = CloudinaryField('image')
+    bio = HTMLField(blank=True,default='Hey There I am using Instagram')
+    
+  
